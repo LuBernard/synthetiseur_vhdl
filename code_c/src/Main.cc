ac5ad4dd03aef79b8c9207d6c97d66ee3b9e6837
bernalud <bernalud@D7010m16011.phelma.grenoble-inp.fr>
2016-12-01 13:11:51 +0100

    modification du main pour montrer a gachet comment git marche

--- code_c/src/Main.cc
+++ code_c/src/Main.cc
@@ -1,8 +1,15 @@
 #include "../lib/Node.h"
+#include "../lib/Mot.h"
+#include "../lib/Entity.h"
 #include <stdio.h>
+#include <iostream.h>
+#include <list.h>
 
 int main(void)
 {
 	printf("hello world");
+	
+		
+	
 	return 0;
 }
